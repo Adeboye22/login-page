ac5be5284cc237b7918174a7ed06391894cd5aeb
auntyemman <1mole.coco@gmail.com>
2023-08-17 12:20:24 +0100

    Error fixed

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,24 +4,28 @@ const userReg = new mongoose.Schema({
     firstname : {
         type : String,
         trim: true,
-        minlength: 2
+        minlength: 2,
+        required: true
     },
     lastname: {
         type: String,
         minlenght: 2,
-        trim: true
+        trim: true,
+        required: true
     },
     email: {
         type: String,
         maxlength: 50,
         unique: true,
-        trim: true
+        trim: true,
+        required: true
     },
     password: {
         type: String,
         maxlength: 100,
         minlenght: 3,
-        trim: true
+        trim: true,
+        required: true
     }
 }, {timestamps: true});
 
